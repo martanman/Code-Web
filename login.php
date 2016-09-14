bc6166bb89e22250f2d65b191b3f661002b283e4
Varun-Sethu <varun.sethu1@education.nsw.gov.au>
2016-09-14 16:27:44 +1000

    stuff

--- login.php
+++ login.php
@@ -4,15 +4,24 @@ session_start();
 
 $email = $_POST['EMAIL']; 
 $password = $_POST['PASS']; 
-    $password = md5($password);    
+    $password = md5($password);
     
 $sql = mysql_query("SELECT * FROM users WHERE email = '".$email."' and password = '".$password."'");
 $count= mysql_num_rows($sql);
 
 
 if($count == 1){
+    
+    //get username from database
     if (!isset ($_SESSION['username'])) {
-    $_SESSION['username'] = implode(mysql_fetch_array(mysql_query("SELECT username FROM users WHERE email = '".$email."' and password = '".$password."'")));
+    $num = mysql_fetch_array(mysql_query("SELECT username FROM users WHERE email = '".$email."' and password = '".$password."'"));
+        foreach($num as $value){
+            $value = $value;
+                $_SESSION['username'] = $value;
+        }
+    }
+    if (!isset ($_SESSION['profilePic'])){
+        
     }
     header("Location: profilepage.php");
 }
--- profilepage.php
+++ profilepage.php
@@ -2,22 +2,46 @@
 session_start();
 $username = $_SESSION['username'];
 ?>
-
 <!DOCTYPE HTML>
 <html>
     <head>
     <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="css/profile.css">
+     <link rel="stylesheet" href="css/profile.css">
     <meta charset="UTF-8">
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet" type="text/css">
     <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/>
     </head>
     <body>
         <div class="banner">
-            <p class="profilename"><?php echo $username; session_destroy(); ?></p>
+           <div class="userinf">
+            <b><p class="profilename"><?php echo $username; ?></p></b>
+            <div class="profilePic"></div>
+            </div>
+            <div class="plus">
+            
+            </div>
+             <div class="gear" onclick="openSettings()"></div>
+            <a href="logout.php" class="logout">Logout</a>
+        </div>
+        <div class="settings" id="settings">
+            <div class="close" onclick="closeSettings()"><p class="x">X</p></div>
+        <form name="settings" class="settingsForm">
+            <input type="text" placeholder="Change Username">
+        </form>
+        
         </div>
     
-    
+    <script type="text/javascript">
+        var settings = document.getElementById("settings");
+        
+        //function openSettings(){
+            //settings.style.visibility="visible";
+        //}
+        //function closeSettings(){
+            //settings.style.visibility = "hidden";
+        //}
+        
+    </script>
     </body>
 
 
